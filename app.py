c54024998dd941ed99e668e296ea56d7b79204b2
matte-trip <matteotripaldi@gmail.com>
2018-11-23 03:04:15 +0100

    23/11 3.04

--- app.py
+++ app.py
@@ -5,41 +5,23 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, ValidationError
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_bootstrap import Bootstrap
 import uuid
 
-from flask_bootstrap import Bootstrap
-
-# Create the application object
 app = Flask(__name__)
-
-# from flask_bootstrap import Bootstrap
-#
-#
-# # unused probably
 bootstrap = Bootstrap(app)
-#
-# # used to secure connection client-side
-app.config['SECRET_KEY'] = 'my secret key'
-#
-# # position of database
+app.config['SECRET_KEY'] = 'secret_key'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
-# creation of database with SQLAlchemy
 db = SQLAlchemy(app)
 
-# login manager initializing
 login_manager = LoginManager(app)
-# this is the login view users will be redirected to if they are not logged in and they try to access a private page
+# Users not logged in who try to access a private page will be redirected to this view
 login_manager.login_view = 'login_registration'
 
 
-# GONNA HAVE TO USE THIS???
-# user_manager = UserManager(app, db, User)
-
-
 # =========================================== MODELS ===========================================
-
 
 # Define User data-model
 class User(UserMixin, db.Model):
@@ -49,8 +31,8 @@ class User(UserMixin, db.Model):
     user_id = db.Column('user_id', db.String(8), nullable=False)
 
     # User Authentication fields
-    email = db.Column('email', db.String(255), primary_key=True, nullable=False, unique=True)
-    password_hash = db.Column('password', db.String(255), nullable=False)
+    email = db.Column('email', db.String(50), primary_key=True, nullable=False, unique=True)
+    password_hash = db.Column('password', db.String(50), nullable=False)
 
     # User fields
     first_name = db.Column('first_name', db.String(50), nullable=False)
@@ -82,7 +64,7 @@ class User(UserMixin, db.Model):
 class LoginForm(FlaskForm):
     login_email = StringField('Email', validators=[DataRequired()])
     login_password = PasswordField('Password', validators=[DataRequired()])
-    login = SubmitField('Login')
+    login_button = SubmitField('Login')
 
     @staticmethod
     def validate_username(email):
@@ -97,7 +79,7 @@ class RegistrationForm(FlaskForm):
     last_name = StringField('Last name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
-    register = SubmitField('Register')
+    registration_button = SubmitField('Register')
 
     @staticmethod
     def validate_username(email):
@@ -106,34 +88,28 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Email does not exist')
 
 
-class EditProfileForm(FlaskForm):
+class EditPrivateDataForm(FlaskForm):
     email = StringField('Email')
     first_name = StringField('First name')
     last_name = StringField('Last name')
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat password', validators=[EqualTo('password')])
-    submit = SubmitField('Submit')
+    submit_button = SubmitField('Submit')
 
 
-class EditBioForm(FlaskForm):
+class EditPublicDataForm(FlaskForm):
     age = IntegerField('Age')
     university = StringField('University')
     bio = TextAreaField('Bio')
     interests = TextAreaField('Interests')
-    save = SubmitField('Save')
+    save_button = SubmitField('Save')
 
 
 # =========================================== VIEWS ===========================================
 
-# This is needed by LoginManager to retrieve a User instance based on its ID (email)
 @login_manager.user_loader
 def get_user(email):
     return User.query.filter_by(email=email).first()
-
-
-# @login_manager.user_loader
-# def get_id(email):
-#     return User.query.filter_by(user_id=email).first()
 
 
 @app.before_first_request
@@ -151,10 +127,9 @@ def home():
 def login_registration():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        # we are certain user exists because of the username validator of LoginForm
+        # User existence check by login_manager.user_loader
         existing_user = get_user(login_form.login_email.data)
         if existing_user.check_password(login_form.login_password.data):
-            # login the user, then redirect to his user page
             login_user(existing_user)
             return redirect(url_for('personal_page'))
 
@@ -166,9 +141,9 @@ def login_registration():
         new_user.last_name = registration_form.last_name.data
         new_user.password = registration_form.password.data
         new_user.user_id = uuid.uuid4().hex[::4].capitalize()
-        # add new user to db
+        # Add new user to db
         db.session.add(new_user)
-        # commit changes to db
+        # Commit changes to db
         db.session.commit()
         login_user(new_user)
         return redirect(url_for('personal_page'))
@@ -179,8 +154,8 @@ def login_registration():
 @app.route('/user', methods=['GET', 'POST'])
 @login_required
 def personal_page():
-    personal_profile_form = EditProfileForm()
-    bio_form = EditBioForm()
+    personal_profile_form = EditPrivateDataForm()
+    bio_form = EditPublicDataForm()
     if personal_profile_form.validate_on_submit():
         if current_user.check_password(personal_profile_form.password.data):
             current_user.email = personal_profile_form.email.data
@@ -189,6 +164,13 @@ def personal_page():
             current_user.password = personal_profile_form.password.data
             db.session.commit()
             return redirect(url_for('personal_page'))
+    if bio_form.validate_on_submit():
+        current_user.age = bio_form.age.data
+        current_user.university = bio_form.university.data
+        current_user.bio = bio_form.bio.data
+        current_user.interests = bio_form.interests.data
+        db.session.commit()
+        return redirect(url_for('personal_page'))
     elif request.method == 'GET':
         personal_profile_form.email.data = current_user.email
         personal_profile_form.first_name.data = current_user.first_name
@@ -197,13 +179,7 @@ def personal_page():
         bio_form.university.data = current_user.university
         bio_form.bio.data = current_user.bio
         bio_form.interests.data = current_user.interests
-    if bio_form.validate_on_submit():
-        current_user.age = bio_form.age.data
-        current_user.university = bio_form.university.data
-        current_user.bio = bio_form.bio.data
-        current_user.interests = bio_form.interests.data
-        db.session.commit()
-        return redirect(url_for('personal_page'))
+
     return render_template('personal_page.html', personal_profile_form=personal_profile_form, bio_form=bio_form)
 
 
