807b1127bcf9fe1d3d15c89f81e068a69f15cdf2
matte-trip <matteotripaldi@gmail.com>
2018-12-03 14:13:44 +0100

    03/12 14.13 lacking: -results -listing -listing that can be modified (new page, analogous) -their implementation in py
    db integration
    one house for every user
    multiple users per house
    search with filters

--- app.py
+++ app.py
@@ -21,7 +21,32 @@ app.config[
     'STATIC_FOLDER'] = 'C:\Users\Matteo\Desktop\Drive\Information Systems\Housr - Information Systems\stairs\static'
 
 app.config['available_cities'] = [("TURIN", "Turin")]
-app.config['boolean_choice'] = [("NO", "No"), ("YES", "Yes")]
+app.config['available_neighbourhoods'] = [("AURORA", "AURORA"),
+                                          ("BARCA", "BARCA"),
+                                          ("BARRIERA DI MILANO", "BARRIERA DI MILANO"),
+                                          ("BORGATA VITTORIA", "BORGATA VITTORIA"),
+                                          ("BORGO PO", "BORGO PO"),
+                                          ("CAMPIDOGLIO", "CAMPIDOGLIO"),
+                                          ("CENISIA", "CENISIA"),
+                                          ("CENTRO", "CENTRO"),
+                                          ("CROCETTA", "CROCETTA"),
+                                          ("FALCHERA", "FALCHERA"),
+                                          ("LANZO", "LANZO"),
+                                          ("LINGOTTO", "LINGOTTO"),
+                                          ("LUCENTO", "LUCENTO"),
+                                          ("MADONNA DEL PILONE", "MADONNA DEL PILONE"),
+                                          ("MIRAFIORI NORD", "MIRAFIORI NORD"),
+                                          ("MIRAFIORI SUD", "MIRAFIORI SUD"),
+                                          ("NIZZA", "NIZZA"),
+                                          ("PARELLA", "PARELLA"),
+                                          ("POZZO STRADA", "POZZO STRADA"),
+                                          ("SAN PAOLO", "SAN PAOLO"),
+                                          ("SAN SALVARIO", "SAN SALVARIO"),
+                                          ("SANTA RITA", "SANTA RITA"),
+                                          ("VANCHIGLIA", "VANCHIGLIA")]
+app.config['available_types'] = [("SINGLE", "SINGLE"),
+                                 ("DOUBLE", "DOUBLE")]
+app.config['boolean_choice'] = [("NO", "No"), ("YES", "Yes")]  # NOT USED???????????????????????????????????????????????
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
@@ -60,7 +85,7 @@ class User(UserMixin, db.Model):
     languages = db.Column('languages', db.String(50))
 
     # Habits
-    habits = db.Column('habits', db.String(8))
+    habits = db.Column('habits', db.String(12))
 
     # Other fields
     photo_id = db.Column('photo_id', db.Integer)
@@ -88,12 +113,15 @@ class Residence(db.Model):
     __tablename__ = 'houses'
 
     # Id field
-    houses_id = db.Column('houses_id', db.String(8), primary_key=True, unique=True, nullable=False)
+    house_id = db.Column('houses_id', db.String(8), primary_key=True, unique=True, nullable=False)
+    house_sc = db.Column('houses_sc', db.String(8), nullable=False)
 
+    type = db.Column('type', db.String(30), nullable=False)
+    name = db.Column('name', db.String(30), nullable=False)  # TITLE = ROOM TYPE + NEIGHBOURHOOD NAME ??????????????????
     city = db.Column('city', db.String(30), nullable=False)
     street = db.Column('street', db.String(50), nullable=False)
     civic = db.Column('civic', db.Integer)
-    neighbourhood = db.Column('neighbourhood', db.String(50), nullable=False)  # SELECTOR OR NOT????????????????????????
+    neighbourhood = db.Column('neighbourhood', db.String(50), nullable=False)  # SELECTOR ??????????????????????????????
     amenities = db.Column('amenities', db.String(8))  # NULLABLE OR NOT?????????????????????????????????????????????????
     description = db.Column('description', db.String(1000), nullable=False)
     house_rules = db.Column('house rules', db.String(1000), nullable=False)
@@ -154,13 +182,13 @@ class EditPublicDataForm(FlaskForm):
 class EditSlidersDataForm(FlaskForm):
     # yes or no
     smoking_habits = DecimalRangeField('Do You Smoke?')
-    past_experience = DecimalRangeField('Do you have past experience of housesharing?')
+    past_experience = DecimalRangeField('Do you have past experiences of house sharing?')
     do_sports = DecimalRangeField('Do you practice sports?')
     pet_friendly = DecimalRangeField('Are you pet friendly?')
     # how much do you like ...
-    eat_together = DecimalRangeField('Consume meals with housemates')
-    ideal_week_end1 = DecimalRangeField('Stay at home and chill')
-    ideal_week_end2 = DecimalRangeField('Hangout with friends')
+    eat_together = DecimalRangeField('Eating with housemates')
+    ideal_week_end1 = DecimalRangeField('Staying at home and chill')
+    ideal_week_end2 = DecimalRangeField('Hanging out with friends')
     house_parties = DecimalRangeField('House parties')
     # i'm used to
     invite_friends = DecimalRangeField('Invite friends at home')
@@ -168,6 +196,33 @@ class EditSlidersDataForm(FlaskForm):
     play_music = DecimalRangeField('Play music without headphones')
     time_at_home = DecimalRangeField('Spend most of my time at home')
     save_habits = SubmitField('Save')
+
+
+class ExistingHouseForm(FlaskForm):
+    house_sc = StringField('Secret House ID')
+    enter_button = SubmitField('Enter')
+
+
+class EditHouseForm(FlaskForm):
+    city = SelectField('City', choices=app.config['available_cities'], validators=[DataRequired()])
+    street = StringField('Street', validators=[DataRequired()])
+    civic = IntegerField('Street', validators=[DataRequired()])
+    neighbourhood = SelectField('Neighbourhood', choices=app.config['available_neighbourhoods'],
+                                validators=[DataRequired()])
+    type = SelectField('Neighbourhood', choices=app.config['available_types'], validators=[DataRequired()])
+
+    house_sc = StringField('Secret House ID')
+
+    house_id = db.Column('houses_id', db.String(8), primary_key=True, unique=True, nullable=False)
+    house_sc = db.Column('houses_sc', db.String(8), nullable=False)
+
+    name = db.Column('name', db.String(30), nullable=False)  # TITLE =
+    amenities = db.Column('amenities', db.String(8))  # NULLABLE OR NOT?????????????????????????????????????????????????
+    description = db.Column('description', db.String(1000), nullable=False)
+    house_rules = db.Column('house rules', db.String(1000), nullable=False)
+    enter_button = SubmitField('Enter')
+
+
 # ======================================================================================================================
 # VIEWS
 # ======================================================================================================================
@@ -355,12 +410,18 @@ def upload():
 def habits():
     habits_form = EditSlidersDataForm()
     if request.method == 'POST':
-        habits_list = [str(habits_form.smoking_habits.data), str(habits_form.past_experience.data),
-                       str(habits_form.eat_together.data), str(habits_form.do_sports.data),
-                       str(habits_form.house_parties.data), str(habits_form.invite_friends.data),
-                       str(habits_form.overnight_guests.data), str(habits_form.play_music.data),
-                       str(habits_form.ideal_week_end1.data), str(habits_form.pet_friendly.data),
-                       str(habits_form.ideal_week_end2.data), str(habits_form.time_at_home.data)]
+        habits_list = [str(habits_form.smoking_habits.data),
+                       str(habits_form.past_experience.data),
+                       str(habits_form.do_sports.data),
+                       str(habits_form.pet_friendly.data),
+                       str(habits_form.eat_together.data),
+                       str(habits_form.ideal_week_end1.data),
+                       str(habits_form.ideal_week_end2.data),
+                       str(habits_form.house_parties.data),
+                       str(habits_form.invite_friends.data),
+                       str(habits_form.overnight_guests.data),
+                       str(habits_form.play_music.data),
+                       str(habits_form.time_at_home.data)]
         current_user.habits = "".join(habits_list)
         db.session.commit()
         return redirect(url_for('personal_page'))
@@ -413,48 +474,6 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-# @app.route('/results')
-# def search_results(search):
-#     results = []
-#     search_string = search.data['search']
-#     if search_string:
-#         if search.data['select'] == 'Artist':
-#             qry = db_session.query(Album, Artist).filter(
-#                 Artist.id==Album.artist_id).filter(
-#                     Artist.name.contains(search_string))
-#             results = [item[0] for item in qry.all()]
-#         elif search.data['select'] == 'Album':
-#             qry = db_session.query(Album).filter(
-#                 Album.title.contains(search_string))
-#             results = qry.all()
-#         elif search.data['select'] == 'Publisher':
-#             qry = db_session.query(Album).filter(
-#                 Album.publisher.contains(search_string))
-#             results = qry.all()
-#         else:
-#             qry = db_session.query(Album)
-#             results = qry.all()
-#     else:
-#         qry = db_session.query(Album)
-#         results = qry.all()
-#     if not results:
-#         flash('No results found!')
-#         return redirect('/')
-#     else:
-#         # display results
-#         table = Results(results)
-#         table.border = True
-#         return render_template('results.html', table=table)
-
-# @app.route('/listing')
-# def listing():
-#     user = User()
-#     user.email = "gino"
-#     user.first_name = "gino"
-#     user.last_name = "gino"
-#     user.password = "gino"
-#     return render_template('listing.html', user=user)
-
 @app.route('/results')
 def search_results():
     # a. Keeps track of user position and shows his pro_pic
@@ -469,48 +488,62 @@ def search_results():
     # this is a fake house ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     new_house = Residence()
     new_house.houses_id = uuid.uuid4().hex[::4].capitalize()
-    new_house.city = "Turino"
+    new_house.city = "Turin"
     new_house.street = "Via Guido"
     new_house.civic = 232
     return render_template('results.html',
                            is_auth=current_user.is_authenticated,
                            pro_pic=pro_pic)
-                          # user=user,# ask mat if this is a
-                          # image_name=user_image_name,
-                          # habits_form=habits_form)
     # this is a fake house ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # -insert the correct data in the return
     # -insert this into the HTML verify the html code for reference
 
 
-@app.route('/h/<house_id>')
-def u(user_id):
-    # a. Keeps track of user position and shows his pro_pic
-    global last_url
-    last_url = "u/" + user_id
+@app.route('/new_house')
+@login_required
+def new_house():
+    pro_pic = current_user.user_id + str(current_user.photo_id) + ".png"
 
-    if current_user.is_authenticated:
-        pro_pic = current_user.user_id + str(current_user.photo_id) + ".png"
-    else:
-        pro_pic = ""
-    # a_end
+    house_form = EditHouseForm()
 
-    user = User.query.filter_by(user_id=user_id.capitalize()).first_or_404()
-    user_image_name = user.user_id + str(user.photo_id) + ".png"
+    if house_form.validate_on_submit():
+        new_house = Residence()
+        new_house.house_id = uuid.uuid4().hex[::4].capitalize()
+        new_house.house_sc = uuid.uuid4().hex[::4].capitalize()
 
-    habits_form = EditSlidersDataForm()
+        new_house.name =  # ROOM TYPE + NEIGHBOURHOOD NAME ??????????????????
 
-    return render_template('listing.html',
-                           house=house,
-                           )
+        new_user.email = registration_form.email.data
+        new_user.first_name = registration_form.first_name.data
+        new_user.last_name = registration_form.last_name.data
+        new_user.city = registration_form.city.data
+        new_user.password = registration_form.password.data
+        new_user.user_id = uuid.uuid4().hex[::4].capitalize()
+        new_user.photo_id = 0
+        new_user.habits = "0000000000"
+        default_image_destination_path = new_user.user_id + "0.png"
+        shutil.copy(os.path.join(app.config['STATIC_FOLDER'], 'user-default.png'),
+                    os.path.join(app.config['UPLOAD_FOLDER'], default_image_destination_path))
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect("http://127.0.0.1:5000/" + last_url)
+
+    house = Residence.query.filter_by(house_id=house_id.capitalize()).first_or_404()
+
+    redirect(url_for('personal_page'))
+
+    return render_template('private_listing.html',
+                           pro_pic=pro_pic,
+                           house_form=house_form)
 
 
 @app.route('/h_edit/<house_id>')
 @login_required
-def u(house_id):
+def h_edit(house_id):
     # a. Keeps track of user position and shows his pro_pic
     global last_url
-    last_url = "h/" +house_id_id
+    last_url = "h/" + house_id
 
     if current_user.is_authenticated:
         pro_pic = current_user.user_id + str(current_user.photo_id) + ".png"
@@ -518,17 +551,11 @@ def u(house_id):
         pro_pic = ""
     # a_end
 
-    user = User.query.filter_by(user_id=user_id.capitalize()).first_or_404()
-    user_image_name = user.user_id + str(user.photo_id) + ".png"
+    house = Residence.query.filter_by(house_id=house_id.capitalize()).first_or_404()
 
-    habits_form = EditSlidersDataForm()
-
-    return render_template('public_profile.html',
-                           user=user,
-                           image_name=user_image_name,
-                           is_auth=current_user.is_authenticated,
+    return render_template('private_listing.html',
                            pro_pic=pro_pic,
-                           habits_form=habits_form)
+                           house=house)
 
 
 @app.route('/upload_house_image/<house_id>', methods=['GET', 'POST'])  # lucas changed ths
@@ -545,7 +572,6 @@ def upload_house_image(house_id):
             print 'No selected file'
             return redirect(request.url)
         if house_picture:
-
             db.session.commit()
             filename = "'\'" + house_id + "\(1).png"
             # saving new image to /uploads
@@ -554,6 +580,47 @@ def upload_house_image(house_id):
             return redirect(url_for('personal_page'))
 
     return render_template('upload.html')
+
+
+@app.route('/h/<house_id>')
+def h(house_id):
+    # a. Keeps track of user position and shows his pro_pic
+    global last_url
+    last_url = "h/" + house_id
+
+    if current_user.is_authenticated:
+        pro_pic = current_user.user_id + str(current_user.photo_id) + ".png"
+    else:
+        pro_pic = ""
+    # a_end
+
+    house = Residence.query.filter_by(house_id=house_id.capitalize()).first_or_404()
+    housemates = User.query.filter_by(house_user_id=house_id.capitalize())
+
+    return render_template('public_listing.html',
+                           pro_pic=pro_pic,
+                           is_auth=current_user.is_authenticated,
+                           house=house,
+                           housemates=housemates)
+
+
+@app.route('/existing')
+@login_required
+def existing():
+    pro_pic = current_user.user_id + str(current_user.photo_id) + ".png"
+
+    existing_house = ExistingHouseForm()
+    # here the user must enter the house secret code in order to join the house
+    house_sc = existing_house.house_sc.data
+    house = Residence.query.filter_by(house_id=house_sc.capitalize()).first_or_404()
+
+    if house:
+        redirect(url_for('personal_page'))
+
+    return render_template('existing.html',
+                           pro_pic=pro_pic,
+                           is_auth=current_user.is_authenticated,
+                           existing_house=existing_house)
 
 
 # ======================================================================================================================
@@ -573,15 +640,6 @@ last_url = ''
 # used to keep track of the last page the user was visiting (public pages)
 # in order to redirect him there after login/logout
 
-#   smoking_habits = "10000000"
-#       vegetarian = "01000000"
-#     eat_together = "00900000"
-#        do_sports = "00090000"
-#    house_parties = "00009000"
-#   invite_friends = "00000900"
-#    stays_in_room = "00000090"
-# overnight_guests = "00000009"
-
 initial___amenities = "00000000"
 amenities______beds = "10000000"
 amenities_____baths = "01000000"
@@ -592,6 +650,8 @@ amenities__bath_tub = "00000100"
 amenities____shower = "00000010"
 amenities_workplace = "00000001"
 
+# ======================================================================================================================
+# STARTUP
 # ======================================================================================================================
 
 if __name__ == '__main__':
