9dfcf18333429642625462906bb0b200243a834a
matte-trip <matteotripaldi@gmail.com>
2018-12-14 15:49:58 +0100

    14/12 15.49 final?

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, redirect, url_for, request, send_from_
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 from flask_wtf import FlaskForm
-from wtforms.fields.html5 import DecimalRangeField
 from wtforms import StringField, IntegerField, SubmitField, PasswordField, TextAreaField, SelectField, BooleanField
+from wtforms.fields.html5 import DecimalRangeField
 from wtforms.validators import DataRequired, EqualTo
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_bootstrap import Bootstrap
